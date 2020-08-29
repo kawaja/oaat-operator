b9ef772fe64b01913e38e87eee1f262123337396
Rik Harris <git@kawaja.net>
2020-08-29 18:03:31 +1000

    add parse_time; change parse_frequency to parse_duration

--- oaat-operator/utility.py
+++ oaat-operator/utility.py
@@ -3,13 +3,14 @@ utility.py
 
 Various stand-alone utility functions.
 """
+from typing import Any
 import datetime
 import re
 import sys
 
 UTC = datetime.timezone.utc
 
-FREQMATCH = re.compile(r'''
+DURMATCH = re.compile(r'''
     \b
     (?P<val>\d+)
     \s*
@@ -21,16 +22,71 @@ FREQMATCH = re.compile(r'''
         w((ee)?ks?)?
     ))($|\b|\d)''', re.VERBOSE)
 
-if not FREQMATCH:
-    raise SyntaxError('FREQMATCH invalid regular expression')
+if not DURMATCH:
+    raise SyntaxError('DURMATCH invalid regular expression')
 
 
-def parse_frequency(freq: str) -> datetime.timedelta:
-    """Calculate timedelta from a frequency string."""
+def parse_time(inputtime: dict) -> datetime.time:
+    """
+    From a dict which contains a 'time' attribute and an optional 'tz'
+    attribute, return a timedelta object which represents the time from
+    midnight.
+    """
+    base = datetime.datetime.strptime('Z', '%z')
+    tz = inputtime.get('tz', 'Z')
+    # +n:00 -> +0n:00
+    if tz[0] == '+':
+        if int(tz[1]) in range(10):
+            if tz[2] == ':':
+                tz = tz[0] + '0' + tz[1:]
+    timestr = inputtime.get('time')
+    if not timestr:
+        return datetime.timedelta(0)
+
+    timestr += tz
+
+    try:
+        timeval = datetime.datetime.strptime(timestr, '%H:%M%z') - base
+    except ValueError:
+        try:
+            timeval = datetime.datetime.strptime(timestr, '%H%z') - base
+        except ValueError:
+            return datetime.timedelta(0)
+
+    # remove the "days" component so time is in the range 00:00 - 23:59
+    return timeval + datetime.timedelta(days=-timeval.days)
+
+
+class TimeWindow:
+    def __init__(self, start: Any, end: Any) -> None:
+        if isinstance(start, str):
+            start = {'time': start}
+        if isinstance(end, str):
+            end = {'time': end}
+        self.start = parse_time(start)
+        self.end = parse_time(end)
+
+    def __contains__(self, testdate: datetime.datetime) -> bool:
+        testtime = testdate.replace(year=1900, month=1, day=1)
+        base = datetime.datetime(year=1900, month=1, day=1, tzinfo=UTC)
+        start = base + self.start
+        end = base + self.end
+        if start > end:
+            if testtime >= start or testtime <= end:
+                return True
+        if testtime >= start and testtime <= end:
+            return True
+        return False
+
+
+def parse_duration(duration: str) -> datetime.timedelta:
+    """Calculate timedelta from a duration string."""
+    if duration is None:
+        return None
     units = {}
     start = 0
     while True:
-        match = FREQMATCH.search(freq[start:])
+        match = DURMATCH.search(duration[start:])
         if not match:
             break
         val = int(match.group('val'))
@@ -50,26 +106,31 @@ def parse_frequency(freq: str) -> datetime.timedelta:
     return datetime.timedelta(**units)
 
 
-def date_from_isostr(datestr):
+def date_from_isostr(datestr: str) -> datetime.datetime:
     """Convert a ISO-format date string to datetime, ensuring UTC."""
     if datestr:
         # fromisoformat() does not recognise trailing Z for UTC
         if datestr[-1:] == 'Z':
             datestr = datestr[:-1] + '+00:00'
-        return datetime.datetime.fromisoformat(datestr).replace(tzinfo=UTC)
+        fromiso = datetime.datetime.fromisoformat(datestr)
+        if fromiso.tzinfo is None:
+            return datetime.datetime.fromisoformat(datestr).replace(tzinfo=UTC)
+        else:
+            return datetime.datetime.fromtimestamp(
+                datetime.datetime.fromisoformat(datestr).timestamp(), tz=UTC)
     return datetime.datetime.fromtimestamp(0, tz=UTC)
 
 
-def now():
+def now() -> datetime.datetime:
     """Current time (in UTC) as datetime."""
     return datetime.datetime.now(tz=UTC)
 
 
-def now_iso():
+def now_iso() -> str:
     """Current time (in UTC) as isoformat string."""
-    now().isoformat()
+    return now().isoformat()
 
 
-def my_name():
+def my_name() -> str:
     """Return the name of the calling function."""
     return sys._getframe(1).f_code.co_name
