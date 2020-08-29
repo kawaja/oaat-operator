a2ab878ebf6d443d4df8d55d21f07be1d7efe4a0
Rik Harris <git@kawaja.net>
2020-08-29 18:01:58 +1000

    add type hints; separate objects for item/type; add cool-off

---
+++ oaat-operator/oaatitem.py
@@ -0,0 +1,126 @@
+"""
+oaatitem.py
+
+Manage OaatItems within an OaatGroup.
+"""
+
+import datetime
+from utility import now_iso, date_from_isostr
+import oaatgroup
+import common
+
+
+# WARNING: this code assumes the pykube object (.obj) and the kopf object
+# both have the same structure
+
+class OaatItems:
+    def __init__(self,
+                 oaatgroupobject: 'oaatgroup.OaatGroupOverseer' = None,
+                 kubeobject: 'common.KubeOaatGroup' = None) -> None:
+        self.oaatgroup = None
+        self.kubeobject = None
+        self.obj = None
+        if oaatgroupobject:
+            self.oaatgroup = oaatgroupobject
+            self.obj = oaatgroupobject.kwargs
+        elif kubeobject:
+            self.kubeobject = kubeobject
+            self.obj = kubeobject.obj
+        else:
+            raise ValueError(
+                'must supply either oaatgroup or kubeobject to OaatItems')
+
+    # uses only common elements of pykube object and kopf object
+    def status(self, name: str, key: str, default: str = None) -> str:
+        """Get the status of an item. """
+        return (self.obj
+                .get('status', {})
+                .get('items', {})
+                .get(name, {})
+                .get(key, default))
+
+    # agnostic of object structure
+    def status_date(self,
+                    item: str,
+                    key: str,
+                    default: str = None) -> datetime.datetime:
+        """Get the status of a specific item, returned as a datetime."""
+        return date_from_isostr(self.status(item, key, default))
+
+    # needs to support pykube object and kopf object separately
+    def set_status(self, item: str, key: str, value: str = None) -> None:
+        """Set the status of a specific item."""
+        if self.oaatgroup:
+            self._set_status_kopf(item, key, value)
+        elif self.kubeobject:
+            self._set_status_pykube(item, key, value)
+
+    def _set_status_kopf(
+            self, item: str, key: str, value: str = None) -> None:
+        patch = (self.oaatgroup.kwargs['patch']['status']
+                 .setdefault('items', {})
+                 .setdefault(item, {}))
+        patch[key] = value
+
+    def _set_status_pykube(
+            self, item: str, key: str, value: str = None) -> None:
+        self.kubeobject.patch({
+            'status': {
+                'items': {
+                    item: {
+                        key: value
+                    }
+                }
+            }
+        })
+
+    # agnostic of object structure
+    def set_phase(self, item: str, value: str) -> None:
+        """Set the phase of a specific item."""
+        self.set_status(item, 'podphase', value)
+
+    # agnostic of object structure
+    def mark_failed(self, name: str, when: str = None) -> None:
+        """Mark an item as failed."""
+        if not when:
+            when = now_iso()
+
+        if not isinstance(when, str):
+            raise ValueError('mark_failed when= should be iso date string')
+
+        failure_count = self.status(name, 'failure_count', 0)
+        self.set_status(name, 'failure_count', failure_count + 1)
+        self.set_status(name, 'last_failure', when)
+
+    # agnostic of object structure
+    def mark_success(self, name: str, when: str = None) -> None:
+        """Mark an item as succeeded."""
+        if not when:
+            when = now_iso()
+
+        if not isinstance(when, str):
+            raise ValueError('mark_failed when= should be iso date string')
+
+        self.set_status(name, 'failure_count', 0)
+        self.set_status(name, 'last_success', when)
+
+    # TODO: provide other mechanisms to get a list of items:
+    #       - output of a container
+    #       - contents of a configmap
+    #       - result of an API call ??
+    # agnostic of object structure
+    def list(self) -> list:
+        """Return the names of all items in a list."""
+        return [
+            {
+                'name': item,
+                'success': self.status_date(item, 'last_success'),
+                'failure': self.status_date(item, 'last_failure'),
+                'numfails': self.status(item, 'failure_count', 0)
+            }
+            for item in self.obj.get('spec', {}).get('oaatItems', [])
+        ]
+
+    # agnostic of object structure
+    def count(self) -> int:
+        return len(self.obj.get('spec', {}).get('oaatItems', []))
--- oaat-operator/overseer.py
+++ oaat-operator/overseer.py
@@ -1,10 +1,11 @@
 """
 overseer.py
 
-Overseer base class.
+Overseer base class for Kopf object processing.
 """
 import time
 import pykube
+from typing import Any
 from common import ProcessingComplete
 
 
@@ -14,43 +15,45 @@ class Overseer:
 
     Base class for managing objects under kopf handler.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         self.kwargs = kwargs
         self.api = pykube.HTTPClient(pykube.KubeConfig.from_env())
-        self.name = kwargs['name']
-        self.namespace = kwargs['namespace']
+        self.name = kwargs.get('name')
+        self.namespace = kwargs.get('namespace')
         self.my_pykube_objtype = None
+        if self.name is None or self.namespace is None:
+            raise ValueError('Overseer must be called with kopf kwargs')
 
-    def error(self, *args):
+    def error(self, *args) -> None:
         """Log an error."""
         self.kwargs['logger'].error(*args)
 
-    def warning(self, *args):
+    def warning(self, *args) -> None:
         """Log a warning."""
         self.kwargs['logger'].warning(*args)
 
-    def info(self, *args):
+    def info(self, *args) -> None:
         """Log an info message."""
         self.kwargs['logger'].info(*args)
 
-    def debug(self, *args):
+    def debug(self, *args) -> None:
         """Log a debug message."""
         self.kwargs['logger'].debug(*args)
 
-    def get_status(self, state, default=None):
+    def get_status(self, state: str, default: str = None) -> Any:
         """Get a value from the "status" of the overseen object."""
         return self.kwargs['status'].get(state, default)
 
-    def set_status(self, state, value=None):
+    def set_status(self, state: str, value: str = None) -> None:
         """Set a field in the "status" of the overseen object."""
         self.kwargs['patch'].setdefault('status', {})
         self.kwargs['patch']['status'][state] = value
 
-    def get_label(self, label, default=None):
+    def get_label(self, label: str, default: str = None) -> str:
         """Get a label from the overseen object."""
         return self.kwargs['meta'].get('labels', {}).get(label, default)
 
-    def get_kubeobj(self, reason=None):
+    def get_kubeobj(self, reason: str = None) -> None:
         """Get the kube object for the overseen object."""
         namespace = self.namespace if self.namespace else pykube.all
         try:
@@ -65,7 +68,7 @@ class Overseer:
                       f': {exc}',
                 message=f'cannot retrieve "{self.name}" object')
 
-    def set_annotation(self, annotation, value=None):
+    def set_annotation(self, annotation: str, value: str = None) -> None:
         """Set or Remove an annotation on the overseen object."""
         myobj = self.get_kubeobj('set annotation')
         if value:
@@ -88,7 +91,7 @@ class Overseer:
             myobj.annotations.pop(f'kawaja.net/{annotation}', None)
             self.debug(f'removed annotation {annotation} from {self.name}')
 
-    def delete(self):
+    def delete(self) -> None:
         myobj = self.get_kubeobj('delete it')
         try:
             myobj.delete(propagation_policy='Background')
@@ -97,7 +100,7 @@ class Overseer:
                 error=f'cannot delete Object {self.name}: {exc}',
                 message=f'cannot delete "{self.name}" object')
 
-    def handle_processing_complete(self, exc):
+    def handle_processing_complete(self, exc: Exception) -> dict:
         if 'state' in exc.ret:
             self.set_status('state', exc.ret['state'])
         if 'info' in exc.ret:
