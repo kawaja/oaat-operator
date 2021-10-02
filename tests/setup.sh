10d0416a17f6bcfdafe4dc570ac4a4b5658aaae2
Rik Harris <git@kawaja.net>
2021-10-02 12:50:13 +1000

    fix OaatItem bug

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=$(dirname $0)/..
 k3d cluster delete
-k3d cluster create
+k3d cluster create --k3s-server-arg '--kube-proxy-arg=conntrack-max-per-core=0' --k3s-agent-arg '--kube-proxy-arg=conntrack-max-per-core=0'
 kubectl apply -f ${dir}/manifests/01-oaat-operator-crd.yaml 
 kubectl apply -f ${dir}/manifests/sample-oaat-type.yaml 
 python3 -m pip install --upgrade pip
