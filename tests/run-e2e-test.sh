6b65ef543f168acc53c9a11ea51d20b5f0162d79
Rik Harris <git@kawaja.net>
2021-10-04 17:04:23 +1100

    added end-to-end test using kuttl

---
+++ tests/run-e2e-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+dir=$(dirname $0)
+dir=$(cd ${dir}; pwd)
+cd ${dir}/..
+if [ "${1}" != "--nocluster" ]; then
+   k3d cluster delete
+   k3d cluster create
+fi
+python3 -m pip install --upgrade pip
+pip --quiet install -r requirements/dev.txt
+cd ${dir}
+find e2e -name '*.yaml' -print0 | xargs -0 -n 1 yq . > /dev/null
+e2e/operator-first/update-steps.sh
+kubectl kuttl test
